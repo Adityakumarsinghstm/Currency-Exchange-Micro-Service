c4a2e3ca06d0e7b0eee7e868f8f2836caec63ce2
Aditya <adityasingh62065@gmail.com>
2024-08-25 22:45:12 +0530

    versioning REST API

---
+++ src/main/java/com/socialMediaApp/versioning/Name.java
@@ -0,0 +1,16 @@
+package com.socialMediaApp.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class Name {
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/socialMediaApp/versioning/PersonV1.java
@@ -0,0 +1,14 @@
+package com.socialMediaApp.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonV1 {
+    private String name;
+}
