f1db9b05e97a6b895950ca644539875743d3e0e7
Aditya <adityasingh62065@gmail.com>
2024-09-28 00:49:56 +0530

    implement UserJpaResource

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(birth_date,id,name)
+values (current_date(),1001,'Aditya');
+
+insert into user_details(birth_date,id,name)
+values (current_date(),1002,'Rohit');
+
+insert into user_details(birth_date,id,name)
+values (current_date(),1003,'Manish');
